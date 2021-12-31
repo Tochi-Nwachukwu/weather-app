20dbc001c10712b41ba1e9e09623f420710a2d0c
Tochi Nwachukwu <tochinwachukwu33@gmail.com>
2021-12-31 09:33:42 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather() {
+    loadingText.style.display = 'block';
+    weatherBox.style.display = 'none';
+    var cityName = searchCity.value;
+    if (cityName.trim().length == 0) {
+        return alert('please enter a city name');
+    }
+    var http = new XMLHttpRequest();
+    var apiKey = '71128d698ca787e0e7b7faea009905d6';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&units=metric&appid=" + apiKey;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function () {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+            var data = JSON.parse(http.responseText);
+            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase
+                ());
+            weatherData.temperature = data.main.temp;
+            updateWeather(weatherData);
+
+        } else if (http.readyState === XMLHttpRequest.DONE) {
+            alert('something went wrong');
+        }
+
+        if (weatherData.description == "OVERCAST CLOUDS") {
+            weatherBox.style.backgroundColor = "red";
+        } 
+        console.log(weatherData)
+    }
+    http.send();
+
+
+
+
+}
+
+function updateWeather(weatherData) {
+    weatherCity.textContent = weatherData.cityName;
+    weatherDescription.textContent = weatherData.description;
+    weatherTemperature.textContent = weatherData.temperature;
+
+    loadingText.style.display = 'none';
+    weatherBox.style.display = 'block';
+}
